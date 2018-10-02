ca3029816dca5f5f94adfecf168d169f8b35566f
kearlias <43573804+kearlias@users.noreply.github.com>
2018-10-02 09:36:03 -0300

    attempted repair to gallery

--- js/imggallery.js
+++ js/imggallery.js
@@ -1,20 +1,22 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+showDivs(slideIndex, modal);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+function plusDivs(n,m) {
+    showDivs(slideIndex += n,m);
 }
 
 function resetDivs(){
   slideIndex = 1;
+
 }
 
-function showDivs(n) {
+function showDivs(n , m) {
     var i;
+    var y = document.getElementsByClassName("modal"+m);
     var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
+    if (n > y.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = y.length} ;
+    for (i = 0; i < y.length; i++) {
         x[i].style.display = "none";
     }
     x[slideIndex-1].style.display = "block";
